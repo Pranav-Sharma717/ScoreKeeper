5c094021d85a0cba506dc821aabb9415b8d5234e
G V Pranav Sharma <pranav.23bey10032@vitbhopal.ac.in>
2025-07-11 00:31:42 +0530

    Debug

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const p2Button = document.querySelector('#p2Button');
 const p2Display = document.querySelector('#p2Display');
 const p1Display = document.querySelector('#p1Display');
 const resetButton = document.querySelector('#reset');
-const maxpointSelect = document.querySelector('#maxpoints')
-
+const maxpointSelect = document.querySelector('#maxpoints');
 
 let p1Score = 0;
 let p2Score = 0;
@@ -13,33 +12,27 @@ let isGameOver = false;
 
 p1Button.addEventListener('click', function () {
     if (!isGameOver) {
-        if (p1Score !== winningScore) {
-            p1Score += 1;
-            p1Display.textContent = p1Score;
-            p1Display.classList.add('winner');
-        }
-        else {
+        p1Score += 1;
+        p1Display.textContent = p1Score;
+        if (p1Score === winningScore) {
             isGameOver = true;
-            p1Display.classList.add('loser');
+            p1Display.classList.add('winner');
+            p2Display.classList.add('loser');
         }
     }
-
-})
+});
 
 p2Button.addEventListener('click', function () {
     if (!isGameOver) {
-        if (p2Score !== winningScore) {
-            p2Score += 1;
-            p2Display.textContent = p2Score;
-            p2Display.classList.add('winner');
-        }
-        else {
+        p2Score += 1;
+        p2Display.textContent = p2Score;
+        if (p2Score === winningScore) {
             isGameOver = true;
+            p2Display.classList.add('winner');
             p1Display.classList.add('loser');
         }
     }
-
-})
+});
 
 resetButton.addEventListener('click', reset);
 
@@ -49,11 +42,11 @@ function reset() {
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
+    p1Display.classList.remove('winner', 'loser');
+    p2Display.classList.remove('winner', 'loser');
 }
-
 
 maxpointSelect.addEventListener('change', function () {
     winningScore = parseInt(this.value);
     reset();
-})
-
+});
